a524db651193c556b5a4c928d4a7f205199a713b
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-01-24 09:46:41 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  array = [mom, father, daughter, son]
+  array = ["Mom", "Father", "Daughter", "Son"]
 end
 
 def add_element_to_end_of_array(array, element)
